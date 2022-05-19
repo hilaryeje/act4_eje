e54d47a473b3180c78432709356de37e94745c62
hilaryeje <101205555+hilaryeje@users.noreply.github.com>
2022-05-20 00:37:28 +0800

    eje2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Add New Product</h2>
+            <h2>Add New Student</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('posts.index') }}"> Back</a>
@@ -29,13 +29,13 @@
      <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Title:</strong>
+                <strong>Student Number:</strong>
                 <input type="text" name="title" class="form-control" placeholder="Enter Title">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Description:</strong>
+                <strong>Details:</strong>
                 <textarea class="form-control" style="height:150px" name="description" placeholder="Enter Description"></textarea>
             </div>
         </div>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Edit Product</h2>
+                <h2>Edit Student</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('posts.index') }}"> Back</a>
